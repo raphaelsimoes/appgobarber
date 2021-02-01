04dae9b68560fe07ac537c94e8e182dad8fc277c
Raphael Faria <raphaelsimoes@gmail.com>
2021-02-01 09:54:19 -0300

    First Commit

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,68 @@
+import React, { useCallback, useRef } from 'react';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logo from '../../assets/logo.png';
+
+import {
+    Container,
+    Title,
+    ForgotPassword,
+    ForgotPasswordText,
+    CreateAccountButton,
+    CreateAccountButtonText
+} from './styles';
+
+const SignIn: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
+
+    const navigation = useNavigation();
+
+    const handleSingIn = useCallback((data: object) => {
+        console.log(data);
+    }, []);
+
+    return (
+        <>
+            <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                enabled
+            >
+                <ScrollView
+                    keyboardShouldPersistTaps="handled"
+                    contentContainerStyle={{ flex:1 }}
+                >
+                    <Container>
+                        <Image source={logo} />
+                        <View>
+                            <Title>Faça seu logon</Title>
+                        </View>
+                        <Form ref={formRef} onSubmit={handleSingIn}>
+                            <Input name="email" icon="mail" placeholder="E-mail"></Input>
+                            <Input name="password" icon="lock" placeholder="Senha"></Input>
+
+                            <Button onPress={() => formRef.current?.submitForm()}>Entrar</Button>
+                        </Form>
+
+                        <ForgotPassword onPress={() => {}}>
+                            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+                        </ForgotPassword>
+                    </Container>
+                </ScrollView>
+            </KeyboardAvoidingView>
+            <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+                <Icon name="log-in" size={20} color="#ff9000" />
+                <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+            </CreateAccountButton>
+        </>
+    );
+};
+
+export default SignIn;
